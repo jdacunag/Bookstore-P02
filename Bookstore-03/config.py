2645af73a6876eee369a1ffa3567fb46b18afe54
jdacunag <jdagii2004@gmail.com>
2025-05-14 19:21:25 -0500

    config and dockerfile for cluster

--- Bookstore-03/config.py
+++ Bookstore-03/config.py
@@ -1,5 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bookstore_user:bookstore_pass@db/bookstore'
-SECRET_KEY = 'secretkey'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+# Usando variables de entorno para configuración
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secretkey')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'mysql+pymysql://bookstore_user:bookstore_pass@db/bookstore')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS', 'False') == 'True'
