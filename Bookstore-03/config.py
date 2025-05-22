377c6aa8a5bc3e3a92ee7bde7c1f8646bcad649b
jdacunag <jdagii2004@gmail.com>
2025-05-22 09:40:43 -0500

    Objectve 3 changes

--- Bookstore-03/config.py
+++ Bookstore-03/config.py
@@ -1,6 +1,5 @@
 import os
 
-# Usando variables de entorno para configuración
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secretkey')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'mysql+pymysql://bookstore_user:bookstore_pass@db/bookstore')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS', 'False') == 'True'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bookstore_user:juandiego123@bookstore-db.cv48yui8kfd3.us-east-1.rds.amazonaws.com/bookstore'
+SECRET_KEY = 'secretkey'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
